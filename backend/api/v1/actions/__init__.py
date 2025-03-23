1313bb17d614c55f2ba06c92e0165c8cefc563cd
zacharyhampton <zachary@zacharysproducts.com>
2025-03-22 19:01:32 -0700

    - session management

--- backend/api/v1/actions/__init__.py
+++ backend/api/v1/actions/__init__.py
@@ -1,22 +1,31 @@
 from fastapi import FastAPI, APIRouter
+from pragmatic_blackjack import Table
 from pydantic import BaseModel
 from .models import StartSession, EndSession
+from ...core.session import Session, active_sessions
 import uuid
 
 router = APIRouter()
 
 @router.post('/start')
 async def start(data: StartSession):
-    #: use grpc to send off start job for
+    #: TODO: integrate ml model and other handles to play game, can register such receiving handles in .ws/__init__.py
+    table = Table(data.table_id, data.session_id)
+    new_session_id = str(uuid.uuid4())
 
-    return {'session_id': str(uuid.uuid4())}
+    active_sessions[new_session_id] = Session(table=table)
+
+    return {'session_id': new_session_id}
 
 
 @router.post('/end')
 async def end(data: EndSession):
-    # if session id from data is in mem cache:
-    # return success true
-    # otherwise return false
+    if data.session_id not in active_sessions:
+        return {'success': False, 'message': 'Session not found.'}
+
+    del active_sessions[data.session_id]
+    #: TODO: end table (standup or disconnect)
+
     return {'success': True}
 
 
--- backend/api/v1/ws/__init__.py
+++ backend/api/v1/ws/__init__.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI, WebSocket, APIRouter
-
+from ...core.session import active_sessions
 
 router = APIRouter()
 
 
 @router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(
+        session_id: str,
+        websocket: WebSocket
+):
     await websocket.accept()
+
+    if session_id not in active_sessions:
+        await websocket.close()
+        return
+
+    session = active_sessions[session_id]
+    session.websocket = websocket
+
     while True:
         await websocket.receive_text()
 
