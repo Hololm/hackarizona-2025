fce89107a9cad0ef45ba2c5daee81a7a898e334c
zacharyhampton <zachary@zacharysproducts.com>
2025-03-22 22:32:15 -0700

    - bug fix

--- backend/api/v1/ws/__init__.py
+++ backend/api/v1/ws/__init__.py
@@ -1,30 +1,35 @@
-from fastapi import WebSocket, APIRouter, WebSocketDisconnect
+from fastapi import WebSocket, APIRouter, WebSocketDisconnect, BackgroundTasks
 from pragmatic_blackjack import Table, Event
 import asyncio
-from .game_handler import GameHandler
+from .game_handler import GameHandler, HandlerBase
+import threading
 
 router = APIRouter()
 
 
-@router.websocket("/v1/ws")
+@router.websocket("/ws")
 async def websocket_endpoint(
         session_id: str,
         table_id: str,
-        websocket: WebSocket
+        websocket: WebSocket,
+        background_task: BackgroundTasks
 ):
     table = Table(table_id, session_id)
     await websocket.accept()
 
     def transfer_messages(event: Event):
-        data = event.model_dump_json()
-        asyncio.create_task(websocket.send_text(data))
+        data = {
+            "event": HandlerBase.__mapping__[type(event)],
+            "data": event.model_dump()
+        }
+        asyncio.create_task(websocket.send_json(data))
 
     table.handle_all(transfer_messages)
 
     handler = GameHandler()
     table.register(handler)
 
-    asyncio.create_task(table.connect())
+    threading.Thread(target=table.connect).start()
 
     try:
         while True:
